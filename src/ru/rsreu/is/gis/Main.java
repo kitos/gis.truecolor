268392e0d65108d225ff8fb88cd51059ed2422ba
Nikita Kirsanov <nikita_kirsanov@epam.com>
2015-05-15 09:45:59 +0300

    masks

--- src/ru/rsreu/is/gis/Main.java
+++ src/ru/rsreu/is/gis/Main.java
@@ -1,8 +1,8 @@
 package ru.rsreu.is.gis;
 
+import ru.rsreu.is.gis.truecolor.SerializationUtils;
 import ru.rsreu.is.gis.truecolor.bmp.BitMapFileHeader;
 import ru.rsreu.is.gis.truecolor.bmp.BitMapInfoHeader;
-import ru.rsreu.is.gis.truecolor.SerializationUtils;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,28 +12,106 @@ import java.nio.file.StandardOpenOption;
 
 public class Main {
 
+    public static final int IMAGE_WIDTH = 640;
+    public static final int IMAGE_HEIGHT = 480;
+
+    private static byte[][] contur = {
+            {0, -1, 0},
+            {-1, 5, -1},
+            {0, -1, 0}},
+            contur2 = {
+                    {-1, -1, -1},
+                    {-1, 9, -1},
+                    {-1, -1, -1}},
+            contur3 = {
+                    {1, -2, 1},
+                    {-2, 4, -2},
+                    {1, -2, 1}};
+
+    private static byte[][] reliefNorth = {
+            {1, 1, 1},
+            {1, -2, 1},
+            {-1, -1, -1}},
+            reliefEast = {
+                    {-1, 1, 1},
+                    {-1, -2, 1},
+                    {-1, 1, 1}},
+            reliefSouth = {
+                    {-1, -1, -1},
+                    {1, -2, 1},
+                    {1, 1, 1}},
+            reliefWest = {
+                    {1, 1, -1},
+                    {1, -2, 1},
+                    {1, 1, -1}};
+
+    private static byte[][][] masks = {contur, contur2, contur3, reliefNorth, reliefEast, reliefSouth, reliefWest};
+
     public static void main(String... args) {
         try {
             Path output = Paths.get("./dst.bmp");
 
             BitMapFileHeader fileHeader = new BitMapFileHeader();
-            fileHeader.bfSize = 640 * 480 * 3 + fileHeader.bfOffbits;
+            fileHeader.bfSize = IMAGE_WIDTH * IMAGE_HEIGHT + fileHeader.bfOffbits + 256 * 4;
+            fileHeader.bfOffbits = 54 + 256 * 4;
             byte[] fileHeaderBytes = SerializationUtils.serialize(fileHeader);
             Files.write(output, fileHeaderBytes);
 
             BitMapInfoHeader infoHeader = new BitMapInfoHeader();
-            infoHeader.biHeight = 480;
-            infoHeader.biWidth = 640;
-            infoHeader.biBitCount = 24;
+            infoHeader.biHeight = IMAGE_HEIGHT;
+            infoHeader.biWidth = IMAGE_WIDTH;
+            infoHeader.biBitCount = 8;
             byte[] infoHeaderBytes = SerializationUtils.serialize(infoHeader);
             Files.write(output, infoHeaderBytes, StandardOpenOption.APPEND);
 
+            byte[] arr = generateColors();
+            Files.write(output, arr, StandardOpenOption.APPEND);
+
             byte[] bytes = Files.readAllBytes(Paths.get(args[0]));
-            process(bytes);
+            bytes = applyMask(bytes, contur2);
             Files.write(output, bytes, StandardOpenOption.APPEND);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static byte[] generateColors() {
+        byte[] arr = new byte[256 * 4];
+        for (int i = 0; i < 256 * 4; i += 4) {
+            byte b = (byte) (i / 4);
+            arr[i] = b;
+            arr[i + 1] = b;
+            arr[i + 2] = b;
+            arr[i + 3] = b;
+        }
+        return arr;
+    }
+
+    private static byte[] applyMask(byte[] bytes, byte[][] mask) {
+        byte[] result = new byte[bytes.length];
+        int sum;
+        for (int i = 0; i < bytes.length; i++) {
+            sum = 0;
+            try {
+                sum += bytes[i - IMAGE_WIDTH - 1] * mask[0][0];
+                sum += bytes[i - IMAGE_WIDTH] * mask[0][1];
+                sum += bytes[i - IMAGE_WIDTH + 1] * mask[0][2];
+
+                sum += bytes[i - 1] * mask[1][0];
+                sum += bytes[i] * mask[1][1];
+                sum += bytes[i + 1] * mask[1][2];
+
+                sum += bytes[i + IMAGE_WIDTH - 1] * mask[2][0];
+                sum += bytes[i + IMAGE_WIDTH] * mask[2][1];
+                sum += bytes[i + IMAGE_WIDTH + 1] * mask[2][2];
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                sum = bytes[i] * mask[1][1];
+            }
+
+            result[i] = (byte) ((sum < 0) ? 0 : ((sum > 255) ? 255 : sum));
+        }
+        return result;
     }
 
     private static void process(byte[] bytes) {
